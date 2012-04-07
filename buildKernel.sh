24f53189449f5bf52f55362db7cf70736330a275
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-06 21:48:56 -0400

    Update kernel configuration and build scripting

--- buildKernel.sh
+++ buildKernel.sh
@@ -13,21 +13,33 @@ TOOLCHAINDIR=/Volumes/android/android-tzb_ics4.0.1/prebuilt/darwin-x86/toolchain
 DROIDGITHUB=TwistedUmbrella/Twisted-Playground.git
 SPADEREPO=/Volumes/android/github-aosp_source/android_device_htc_ace
 SPADEGITHUB=ThePlayground/android_device_htc_ace.git
+ICSREPO=/Volumes/android/github-aosp_source/android_system_core
+SPDTWKR=/Volumes/android/Twisted-Playground/ScriptFusion
+MSMREPO=/Volumes/android/github-aosp_source/android_device_htc_msm7x30-common
 zipfile=$HANDLE"_Andromadus-hijack_ICS.zip"
 
 CPU_JOB_NUM=16
-TOOLCHAIN_PREFIX=$TOOLCHAINDIR/arm-eabi-
+TOOLCHAIN_PREFIX=arm-none-eabi-
 
 echo "Config Name? "
 ls config
 read configfile
 cp -R config/$configfile .config
 
+cp -R $ICSREPO/rootdir/init.rc $KERNELSPEC/mkboot.aosp/boot.img-ramdisk
+cp -R $ICSREPO/rootdir/ueventd.rc $KERNELSPEC/mkboot.aosp/boot.img-ramdisk
+cp -R $SPDTWKR/speedtweak.sh $KERNELSPEC/mkboot.aosp/boot.img-ramdisk/sbin
+cp -R $SPADEREPO/kernel/init.spade.rc $KERNELSPEC/mkboot.aosp/boot.img-ramdisk
+cp -R $SPADEREPO/kernel/ueventd.spade.rc $KERNELSPEC/mkboot.aosp/boot.img-ramdisk
+cp -R $MSMREPO/kernel/init.msm7x30.usb.rc $KERNELSPEC/mkboot.aosp/boot.img-ramdisk
+
 make clean -j$CPU_JOB_NUM
 
 make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN_PREFIX
 
 find . -name "*.ko" | xargs ${TOOLCHAIN_PREFIX}strip --strip-unneeded
+
+cp .config arch/arm/configs/lean_aosp_defconfig
 
 if [ -e arch/arm/boot/zImage ]; then
 
@@ -38,36 +50,40 @@ echo "adding to build"
 cp -R arch/arm/boot/zImage $SPADEREPO/kernel/kernel
 rm -r $SPADEREPO/kernel/lib/modules/*
 for j in $(find . -name "*.ko"); do
-cp "${j}" $SPADEREPO/kernel/lib/modules
+cp -R "${j}" $SPADEREPO/kernel/lib/modules
 done
 
 cd $SPADEREPO
 git commit -a -m "Automated Kernel Update - ${PROPER}"
-git push git@github.com:$SHOOTGITHUB HEAD:ics -f
+git push git@github.com:$SPADEGITHUB HEAD:ics -f
 
 else
 
-rm -fr tmpdir
-mkdir tmpdir
-cp arch/arm/boot/zImage tmpdir/
-for j in $(find . -name "*.ko"); do
-    cp "${j}" tmpdir/
-done
+if [ ! -e zip.aosp/system/lib ]; then
+mkdir zip.aosp/system/lib
+fi
+if [ ! -e zip.aosp/system/lib/modules ]; then
+mkdir zip.aosp/system/lib/modules
+else
+rm -r zip.aosp/system/lib/modules
+mkdir zip.aosp/system/lib/modules
+fi
 
-cp -a anykernel.tpl tmpdir/anykernel
-mkdir -p tmpdir/anykernel/kernel
-mkdir -p tmpdir/anykernel/system/lib/modules
-cp tmpdir/zImage tmpdir/anykernel/kernel
-for j in tmpdir/*.ko; do
-    cp "${j}" tmpdir/anykernel/system/lib/modules/
+for j in $(find . -name "*.ko"); do
+cp -R "${j}" zip.aosp/system/lib/modules
 done
+cp -R arch/arm/boot/zImage mkboot.aosp
+
+cd mkboot.aosp
+echo "making boot image"
+./img.sh
 
 echo "making zip file"
-cd tmpdir/anykernel
+cp -R boot.img ../zip.aosp
+cd ../zip.aosp
+rm *.zip
 zip -r $zipfile *
-cp -R $zipfile $ANDROIDREPO/Kernel/$zipfile
-cd ../../
-rm -fr tmpdir
+cp -R $KERNELSPEC/zip.aosp/$zipfile $ANDROIDREPO/Kernel/$zipfile
 cd $ANDROIDREPO
 git checkout gh-pages
 git commit -a -m "Automated Patch Kernel Build - ${PROPER}"
